435151e80b817d5ac11e0af4f4b93f214dd26ecf
Johnny <jasiek0006@gmail.com>
2024-11-18 20:59:29 +0100

    Spring | project init

---
+++ zto-spring/src/main/java/wsb/merito/zto/ZtoApplication.java
@@ -0,0 +1,13 @@
+package wsb.merito.zto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtoApplication.class, args);
+	}
+
+}
---
+++ zto-spring/src/test/java/wsb/merito/zto/ZtoApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.merito.zto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
