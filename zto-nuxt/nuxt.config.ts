b1081df655cb375d3ac08440ca25daa43d2f4fda
Mateusz <mateusz.gabryel@carrier.com>
2024-12-11 16:06:34 +0100

    Add Pinia and oFetch modules with configuration | Nuxt

--- zto-nuxt/nuxt.config.ts
+++ zto-nuxt/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  modules: ['@pinia/nuxt'],
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:3001/api',
+    },
+  },
 })
