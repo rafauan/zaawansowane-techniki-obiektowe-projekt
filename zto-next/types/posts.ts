b70c5401a1728203f94e44c5167752d1fce2475a
Stanisław Białecki <stasiu.bialecki@gmail.com>
2024-12-30 23:42:19 +0100

    chore: [Next-js] setup globally persisted store & types

--- zto-next/types/posts.ts
+++ zto-next/types/posts.ts
@@ -1,36 +1,43 @@
-interface Post {
-	id: number;
-	likes: number;
-	is_pinned: boolean;
-	user_id: number;
-	content: string;
-	title: string;
-	updated_at: string;
-	created_at: string;
+export interface IPost {
+  id: number;
+  likes: number;
+  is_pinned: boolean;
+  user_id: number;
+  content: string;
+  title: string;
+  updated_at: string;
+  created_at: string;
 }
 
-type Posts = Post[];
+export interface CreatePostResponse {
+  message: string;
+  post: IPost;
+}
 
-type PostsResponse = {
-	message: string;
-	posts: Posts;
+export interface UpdatePostResponse {
+  id: number;
+  title: string;
+  content: string;
+  updatedAt: string;
+}
+
+export interface GetPostsResponse {
+  message: string;
+  posts: IPost[];
+}
+
+export interface DeletePostResponse {
+  message: string;
+}
+
+//Payloads
+export type CreatePostPayload = {
+  title: string;
+  content: string;
 };
 
-type CreatePostPayload = {
-	title: string;
-	content: string;
-};
-
-type UpdatePostPayload = {
-	title: string;
-	content: string;
-	postId: number;
-};
-
-export type {
-	Post,
-	Posts,
-	PostsResponse,
-	CreatePostPayload,
-	UpdatePostPayload,
+export type UpdatePostPayload = {
+  title: string;
+  content: string;
+  postId: number;
 };
